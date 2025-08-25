c9d20822b03471a10e1e2e76eca91205847e2276
Joonwoo Hwang <wnsdd1@gmail.com>
2025-08-26 08:15:03 +0900

    feat : Subsystem 기반 Manager 추가
    - Quest,  Item, Interaction, DataTable
    - 기본 뼈대 구축

---
+++ Source/ARPG_ContetDemo/Public/Manager/ACDItemManager.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "ACDItemManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_CONTETDEMO_API UACDItemManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+};
