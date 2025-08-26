f26f5bc80e02786d2f7d2528cc43d98b4fb1ea66
Joonwoo Hwang <wnsdd1@gmail.com>
2025-08-27 00:56:38 +0900

    feat : Character에 인터랙션 기본 대응 추가

---
+++ Source/ARPG_ContentDemo/Private/Character/ACDCharacter.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/ACDCharacter.h"
+#include "Component/ACDInteractionSensorComponent.h"
+
+// Sets default values
+AACDCharacter::AACDCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AACDCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AACDCharacter::OnTargetChanged(AActor* NewTarget, const FText& PromptText)
+{
+	const bool bShow = (NewTarget != nullptr);
+	BP_UpdateInteractPrompt(bShow, PromptText);
+}
---
+++ Source/ARPG_ContentDemo/Public/Character/ACDCharacter.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ARPG_ContentDemo/ThirdPersonSample/ARPG_ContentDemoCharacter.h"
+#include "ACDCharacter.generated.h"
+
+class UACDInteractionSensorComponent;
+
+UCLASS()
+class ARPG_CONTENTDEMO_API AACDCharacter : public AARPG_ContentDemoCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AACDCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION()
+	void OnTargetChanged(AActor* NewTarget, const FText& PromptText);
+
+	UFUNCTION(BlueprintImplementableEvent, Category="UI")
+	void BP_UpdateInteractPrompt(bool bShow, const FText& PromptText);
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Interaction")
+	UACDInteractionSensorComponent* InteractionSensor = nullptr;
+};
