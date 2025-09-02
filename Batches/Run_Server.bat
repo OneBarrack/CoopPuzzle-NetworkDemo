7eba64749685914faee8ee78d2bf43f5c37f6bc9
Joonwoo Hwang <wnsdd1@gmail.com>
2025-09-03 01:32:17 +0900

    refactor & fix : 맵 변경 및 Server packaging 실패 트러블슈팅
    - Main맵을 MAP_ARPGContentDemo_Main로 변경
    - 프로젝트세팅(맵&모드 설정, 패키징 맵 변경) 및 기존 맵 경로 참조 부분 수정(ini, Batch)
    - Server packaging 실패 원인 분석
      ->프로젝트 Engine 베이스가 자체 제공 버전으로 재설정 되었던 부분이 문제로, 소스코드 베이스로 변경

--- Batches/Run_Server.bat
+++ Batches/Run_Server.bat
@@ -1,6 +1,6 @@
 @echo off
 set BIN=D:\UnrealProjects\ARPG_ContentDemo\Packaged\WindowsServer\ARPG_ContentDemo\Binaries\Win64
-set MAP=/Game/ThirdPerson/Maps/ThirdPersonMap
+set MAP=/Game/Maps/Map_ARPGContentDemo_Main
 set PORT=7777
 
 "%BIN%\ARPG_ContentDemoServer.exe" "%MAP%" -log -port=%PORT% -unattended -abslog
